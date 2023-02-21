301ac63bf00c84ac03c270cb2e1227fc9eae3a8a
greyglamsjp <trquan@glamscorp.jp>
2023-02-21 10:58:21 +0700

    update ui

--- src/components/Bar/Header.js
+++ src/components/Bar/Header.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Styled Components
-import { BiMenu } from 'react-icons/bi';
-
-// Custom Components
-import SearchBar from '../Search/SearchBar';
+import { BiMenu } from "react-icons/bi";
 
 // Link for linking to other page
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // Assets
-import Logo from '../../assets/logo192.png';
+import Logo from "../../assets/logo192.png";
 
 const Header = () => {
   const [show, setShow] = useState(false);
@@ -18,57 +15,51 @@ const Header = () => {
   return (
     <div>
       <div>
-        <div className='header'>
-          <div className='brand'>
+        <div className="header">
+          <div className="brand">
             <BiMenu
-              className='menu-icon'
+              className="menu-icon"
               onClick={() => setShow(!show)}
               size={25}
             />
-            <img src={Logo} alt='Logo' />
-            <h4>Notes Taking App</h4>
-          </div>
-          <div className='search-bar'>
-            <SearchBar />
+            <img src={Logo} alt="Logo" />
+            <h4>Notes Taking</h4>
           </div>
         </div>
 
         <div
-          className='mobile-menu'
-          style={{ display: show ? 'block' : 'none' }}
+          className="mobile-menu"
+          style={{ display: show ? "block" : "none" }}
         >
-          <Link className='menu-item' to='/'>
+          <Link className="menu-item" to="/">
             Notes
           </Link>
-          <Link className='menu-item' to='/archieve'>
+          <Link className="menu-item" to="/archieve">
             Archive
           </Link>
-          <Link className='menu-item' to='/bin'>
+          <Link className="menu-item" to="/bin">
             Bin
           </Link>
         </div>
-        <div className='mobile-search'>
-          <SearchBar />
-        </div>
       </div>
 
-      <nav className='nav__cont'>
-        <ul className='nav'>
-          <Link to='/'>
-            <li className='nav__items '>
-              <i className='fas fa-lightbulb'></i>
+      <nav className="nav__cont">
+        <ul className="nav">
+          <Link to="/">
+            <li className="nav__items ">
+              <i className="fas fa-lightbulb"></i>
               <p>Notes</p>
             </li>
           </Link>
-          <Link to='/archieve'>
-            <li className='nav__items '>
-              <i className='fas fa-archive'></i>
+          <Link to="/archieve">
+            <li className="nav__items ">
+              <i className="fas fa-archive"></i>
               <p>Archive</p>
             </li>
           </Link>
-          <Link to='/bin'>
-            <li className='nav__items '>
-              <i className='far fa-trash-alt'></i>
+          <Link to="/bin">
+            <li className="nav__items ">
+              <i className="far fa-trash-alt"></i>
               <p>Bin</p>
             </li>
           </Link>
